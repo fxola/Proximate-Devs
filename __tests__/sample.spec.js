8036e5e360967e774decb9c662fdf40156cc5fa6
fxola <afolabiabass62@gmail.com>
2019-09-12 20:36:35 +0100

    chore(circleci): setup react native app, codeclimate, circleci and eslint

--- __tests__/sample.spec.js
+++ __tests__/sample.spec.js
@@ -1,5 +1,16 @@
-describe('First test', () => {
-  it('Should pass', () => {
-    expect(2 + 2).toBe(4);
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../App';
+
+const props = {
+  navigation: {
+    navigate: jest.fn()
+  }
+};
+describe('<App />', () => {
+  const tree = shallow(<App {...props} />);
+  it('Should render correctly', () => {
+    expect(tree).toMatchSnapshot();
   });
 });
